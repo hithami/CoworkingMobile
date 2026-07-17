82dd44bd3198c919af4c1d07431fff2731aaf4dc
Thamires Maiata <thamiresmaiata2024@gmail.com>
2026-07-16 21:13:46 -0300

    feat: adiciona listarAgendamento editarAgendamento e excluirAgendamento ao arquivo agendamento_repository

--- sistema_coworking_flutter/lib/repositories/agendamento_repository.dart
+++ sistema_coworking_flutter/lib/repositories/agendamento_repository.dart
@@ -13,7 +13,54 @@ class AgendamentoRepository {
     await db.insert( 
       'agendamento', 
       agendamento.toMap() 
-      );
+    );
+  } 
+   
+  //metodo listar 
+  Future<List<Agendamento>> listarAgendamento() async { 
+     
+    final db = await banco.conexao; 
+     
+    final List<Map<String, dynamic>> resultado = await db.rawQuery( 
+      ''' 
+      SELECT 
+        agendamento.id_agendamento, 
+        agendamento.data_hora_inicio, 
+        agendamento.data_hora_fim, 
+        agendamento.id_sala, 
+        sala.nome_sala 
+      FROM agendamento agendamento 
+      INNER JOIN sala sala 
+        ON agendamento.id_sala = sala.id_sala 
+      '''  
+    ); 
+     
+    return resultado.map((item) => Agendamento.fromMap(item)).toList();
+  } 
+   
+  //metodo editar 
+  Future<void> editarAgendamento(Agendamento agendamento) async { 
+     
+    final db = await banco.conexao; 
+     
+    await db.update( 
+      'agendamento', 
+      agendamento.toMap(), 
+      where: 'id_agendamento = ?', 
+      whereArgs: [agendamento.idAgendamento],
+    ); 
+  } 
+   
+  //metodo excluir 
+  Future<void> excluirAgendamento(int idAgendamento) async { 
+     
+    final db = await banco.conexao; 
+     
+    await db.delete( 
+      'agendamento', 
+      where: 'id_agendamento = ?', 
+      whereArgs: [idAgendamento],
+    );
   }
 }
- 
+
