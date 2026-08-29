4df6c24ae94c0cc35f691c0ae3340a7243dfef5b
Thamires Maiata <thamiresmaiata2024@gmail.com>
2026-08-29 19:53:21 -0300

    feat: adiciona carregamento de salas para dropdown de agendamento

--- sistema_coworking_flutter/lib/screens/agendamento_screen.dart
+++ sistema_coworking_flutter/lib/screens/agendamento_screen.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
+import '../models/sala.dart';
+import '../repositories/sala_repository.dart';
 
 class AgendamentoScreen extends StatefulWidget {
   const AgendamentoScreen({super.key});
@@ -8,6 +10,25 @@ class AgendamentoScreen extends StatefulWidget {
 }
 
 class _AgendamentoScreenState extends State<AgendamentoScreen> {
+   
+  final SalaRepository _salaRepository = SalaRepository();
+
+  List<Sala> _salas = [];
+
+  Sala? _salaSelecionada;
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarSalas();
+  }
+
+  Future<void> _carregarSalas() async {
+    final salas = await _salaRepository.listarSalas();
+    setState(() {
+      _salas = salas;
+    });
+  } 
 
   @override
   Widget build(BuildContext context) {
